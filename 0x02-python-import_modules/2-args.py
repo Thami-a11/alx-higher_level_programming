d8a14ff0b1f5663a4d53a8518eabfee1706d2ff1
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2024-01-04 22:19:18 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     print("0. arguments")
   elif counter == 1:
     print("1. argument:")
+    print("{}: {}".format(counter, sys.argv[counter + 1]))
   else:
-     print("{} arguments:".format(counter))
+    print("{} arguments:".format(counter))
     for i in range(counter):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+      print("{}: {}".format(i + 1, sys.argv[i + 1]))
