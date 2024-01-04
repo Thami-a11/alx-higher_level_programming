97a7cbacc7b3cf520b8d99c80cbd72b2c4d6da84
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2024-01-04 22:32:55 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
   """Add all interger from args"""
   import sys
   n = 0
-  for i in range(len(sys.argv)):
+  for i in range(len(sys.argv) - 1):
     n += int(sys.argv[i + 1])
   print(n)
