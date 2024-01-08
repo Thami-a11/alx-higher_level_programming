a8c5a36ea08bf4439b3a20f96445119a7c1eb299
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2024-01-09 00:25:52 +0200

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,4 +5,4 @@ def element_at(my_list, idx):
     return null
   elif idx > len(my_list):
     return null
-  print("Element of {} is {}".format(idx,my_list[idx])
+  print("Element of {:d} is {}".format(idx,my_list[idx])
