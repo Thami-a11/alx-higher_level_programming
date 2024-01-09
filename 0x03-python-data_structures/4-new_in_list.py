1f0542dcf6d52183d7f6f524d7ad846c6624b8ee
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2024-01-09 20:41:10 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return(my_list)
+    elif idx > len(my_list) - 1:
+         return(my_list)
+    newlist = my_list
+    newlist[idx] = element
+    return(newlist)
