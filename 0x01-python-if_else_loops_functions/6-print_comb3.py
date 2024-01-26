f95c1069827cfab0cdb0f31f0b725ecafe3dc136
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2024-01-26 21:13:15 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i:02d}, {j:02d}", end=", " * (j < 9) or "\n")
+for d1 in range(0, 10):
+    for d2 in range(d1 + 1, 10):
+        if d1 == 8 and d2 == 9:
+            print("{}{}".format(d1, d2))
+        else:
+            print("{}{}".format(d1, d2), end=", ")
